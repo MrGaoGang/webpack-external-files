a329a489883836e11b6012f74dc13aa52a9269d2
mr.gao <gaogangwork@qq.com>
2020-02-29 09:24:39 +0800

    chore: 初始化demo

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
+// 此处手工import只是作为样例使用
 import { widgets } from "../dist-lib/webpack-external-files.umd.js";
 console.log(widgets, "=========");
 
